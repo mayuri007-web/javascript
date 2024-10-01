916c85567557ca55c4fdd458d04b68ab32b0830e
mayuri007-web <mayurinema@bitcot.com>
2024-10-01 11:39:02 +0000

    update code

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,5 +1,11 @@
 const employeeId = 123456;
 let employeeEmail = "123@gmail.com";
 var employeenum = "1234567890";
-employeeSalary = 300000;
-console.table([employeeId,employeeEmail,employeenum,employeeSalary]);
+employeeSalary = 300000; //wrong way to write a code
+let management;
+
+// employeeId = 12345334;
+employeeEmail = "1234@gmail.com";
+employeenum = "0987654321";
+// employeeSalary = 500000;
+console.table([employeeId,employeeEmail,employeenum,employeeSalary,management]);
