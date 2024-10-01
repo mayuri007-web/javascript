eb42ac569ad868f99f59eae876bccc1747806719
mayuri007-web <mayurinema@bitcot.com>
2024-10-01 07:59:04 +0000

    learn variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,5 @@
+const employeeId = 123456;
+let employeeEmail = "123@gmail.com";
+var employeenum = "1234567890";
+employeeSalary = 300000;
+console.table([employeeId,employeeEmail,employeenum,employeeSalary]);
