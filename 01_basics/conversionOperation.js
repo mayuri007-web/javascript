1a7a0f4b72064a2e866961805ac58b9d591feedb
mayuri007-web <mayurinema@bitcot.com>
2024-10-03 07:02:42 +0000

    conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,11 @@
+let score = false
+// console.log(typeof valueinNumber);
+
+let valueinNumber = Number(score);
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+
+let val = 1
+let valinBoolean = Boolean(val);
+console.log (typeof valinBoolean);
+console.log (valinBoolean);
