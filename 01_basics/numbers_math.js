de43b5ebe44f886af7dfc28980edd68ca56eb02b
mayuri007-web <mayurinema@bitcot.com>
2024-10-07 11:40:15 +0000

    math & number

---
+++ 01_basics/numbers_math.js
@@ -0,0 +1,8 @@
+console.log(Math);
+console.log(Math.abs(5)); //abs -> absolute value it only convert negative value to the positiva value
+console.log(Math.round(6.444));
+console.log(Math.ceil(9.2))
+console.log(Math.floor(6.7))
+console.log(Math.min(1,2,3,4))
+console.log(Math.max(3,6,7,8))
+console.log(Math.floor (Math.random()*10) +1)
