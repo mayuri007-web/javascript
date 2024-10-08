660d8e69ec72c071378b34ddf7945d3ff326e626
mayuri007-web <mayurinema@bitcot.com>
2024-10-08 07:53:52 +0000

    date and time

---
+++ 01_basics/date_and_time.js
@@ -0,0 +1,15 @@
+// let mydate = new Date();
+// console.log(mydate.toString());
+// console.log(mydate.toUTCString());
+// console.log(typeof mydate)
+
+// if i want to print my time
+
+// let mynewdate = new Date (2003,3,15,12,5,55);
+// console.log(mynewdate.toString());
+// let myynewdate = new Date ("2003-6-24");
+// console.log(myynewdate.toLocaleString());
+
+// let myTimrStemp = Date.now()
+// console.log(myTimrStemp)
+console.log(Math.floor(Date.now()/1000))
